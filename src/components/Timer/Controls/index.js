acd53ec64e4517ff133a36d8e2e576cef0a96302
memowii <memogl25@gmail.com>
2020-09-04 22:41:37 -0500

    Add Controls component

---
+++ src/components/Timer/Controls/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./index.css";
+
+export const Controls = () => (
+  <div className="Controls">
+    <button className="btn btn-success btn-lg btn-block rounded-0">START</button>
+  </div>
+);
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./index.css";
 import { Display } from "./Display";
 import { Keypad } from "./Keypad";
+import { Controls } from "./Controls";
 
 export function Timer() {
   return (
     <div className="Timer">
       <Display />
       <Keypad />
+      <Controls />
     </div>
   );
 }
