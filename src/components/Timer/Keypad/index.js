38af2af1f0f4f51f8adcd7bcc5fc46a88f1fd80f
memowii <memogl25@gmail.com>
2020-09-04 19:53:11 -0500

    Add component Keypad

---
+++ src/components/Timer/Keypad/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./index.css";
+
+export const Keypad = () => (
+  <div className="Keypad">
+    <button className="btn btn-light btn-sm">1</button>
+    <button className="btn btn-light btn-sm">2</button>
+    <button className="btn btn-light btn-sm">3</button>
+    <button className="btn btn-light btn-sm">4</button>
+    <button className="btn btn-light btn-sm">5</button>
+    <button className="btn btn-light btn-sm">6</button>
+    <button className="btn btn-light btn-sm">7</button>
+    <button className="btn btn-light btn-sm">8</button>
+    <button className="btn btn-light btn-sm">9</button>
+    <button className="btn btn-light btn-sm">x</button>
+    <button className="btn btn-light btn-sm">0</button>
+    <button className="btn btn-light btn-sm">&nbsp;</button>
+  </div>
+);
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./index.css";
 import { Display } from "./Display";
+import { Keypad } from "./Keypad";
 
 export function Timer() {
   return (
     <div className="Timer">
       <Display />
+      <Keypad />
     </div>
   );
 }
